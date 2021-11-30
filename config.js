cab881d2cf85c455d4400d04102eb7e1d437d197
pandorum2096 <49597462+pandorum2096@users.noreply.github.com>
2021-11-30 13:25:53 +0000

    commit heroku

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "TOUre+2096",
-    database: "entreprise",
-    host: "localhost",
+    user: "jefmspteharqdn",
+    password: "bcda48859599e49a05ea7c4e90913798d6ffac26bfa64416bc787c318f8d1bda",
+    database: "dih9o4blv40v0",
+    host: "ec2-44-193-111-218.compute-1.amazonaws.com",
     port: 5432
 });
 

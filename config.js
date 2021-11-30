8c751a02d1a129001f24ad2aaf230438d281b51a
pandorum2096 <49597462+pandorum2096@users.noreply.github.com>
2021-11-30 14:46:21 +0000

    commit

--- config.js
+++ config.js
@@ -1,37 +1,11 @@
 const Pool = require("pg").Pool;
 
-const config = {
-  db: {
+const config = new Pool({
     user: "jefmspteharqdn",
     password: "bcda48859599e49a05ea7c4e90913798d6ffac26bfa64416bc787c318f8d1bda",
-    database: "dih9o4blv40v0",  
+    database: "dih9o4blv40v0",
     host: "ec2-44-193-111-218.compute-1.amazonaws.com",
     port: 5432
-  }
-};
-
-// const config = {
-//   db: { /* do not put password or any sensitive info here, done only for demo */
-//     host: env.DB_HOST || 'otto.db.elephantsql.com',
-//     port: env.DB_PORT || '5432',
-//     user: env.DB_USER || 'cklijfef',
-//     password: env.DB_PASSWORD || 'V1qidES5k3DSJICDRgXtyT8qeu2SPCZp',
-//     database: env.DB_NAME || 'cklijfef',
-//   },
-//   listPerPage: env.LIST_PER_PAGE || 10,
-// };
-
-
-// const Pool = require("pg").Pool;
-
-// const pool = new Pool({
-//     user: "jefmspteharqdn",
-//     password: "bcda48859599e49a05ea7c4e90913798d6ffac26bfa64416bc787c318f8d1bda",
-//     database: "dih9o4blv40v0",
-//     host: "ec2-44-193-111-218.compute-1.amazonaws.com",
-//     port: 5432
-// });
-
-// module.exports = pool;
+});
 
 module.exports = config;

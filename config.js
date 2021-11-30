170c7ee388b03a8e5fefb54280693bae81e80ec7
pandorum2096 <49597462+pandorum2096@users.noreply.github.com>
2021-11-30 15:21:55 +0000

    commit

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = new Pool({
     user: "jefmspteharqdn",
     password: "bcda48859599e49a05ea7c4e90913798d6ffac26bfa64416bc787c318f8d1bda",
     database: "dih9o4blv40v0",
-    host: "18.207.212.176",
+    host: "ec2-44-193-111-218.compute-1.amazonaws.com",
     port: 5432
 });
 
